e1a7ef4ce5daf66293fc657b1fd25369d41ef499
SuHaye <patricksuchy2@gmail.com>
2021-07-08 00:51:54 -0500

    Added SQL database and models for data

---
+++ food_tracker/extensions.py
@@ -0,0 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Create instance?
+db = SQLAlchemy()
---
+++ food_tracker/models.py
@@ -0,0 +1,24 @@
+from .extensions import db
+
+
+# Create relationship between logdate and food
+log_food = db.Table('log_food',
+    db.Column('log_id', db.Integer, db.ForeignKey('log.id'), primary_key=True),
+    db.Column('food_id', db.Integer, db.ForeignKey('food.id'), primary_key=True)
+)
+
+
+# Model for food and what values it'll have
+class Food(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    proteins = db.Column(db.Integer, nullable=False)
+    carbs = db.Column(db.Integer, nullable=False)
+    fats = db.Column(db.Integer, nullable=False)
+
+
+# Model for dates
+class Log(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    foods = db.relationship('Food', secondary=log_food)
