d22783c57e8c5048c004952ee3d7eaf93f1de133
Patrick S <patricksuchy2@gmail.com>
2021-07-12 20:11:23 -0500

    Added delete and edit button functionality

--- food_tracker/main/routes.py
+++ food_tracker/main/routes.py
@@ -15,7 +15,7 @@ def add():
     # list of all foods in database
     foods = Food.query.all()
 
-    return render_template('add.html', foods=foods)
+    return render_template('add.html', foods=foods, food=None)
 
 # Linked to form that's submited, this method is ran when submitted
 @main.route('/add', methods=['POST'])
@@ -26,18 +26,50 @@ def add_post():
     carbs = request.form.get('carbohydrates')
     fats = request.form.get('fat')
 
-    # creates instance of food model
-    new_food = Food(
-        name=food_name,
-        proteins=proteins,
-        carbs=carbs,
-        fats=fats)
+    # linked to hidden input
+    food_id = request.form.get('food-id')
 
-    db.session.add(new_food) #pushes instance of food model
+    if food_id:
+        # updates existing food if it is there
+        food = Food.query.get(food_id)
+        food.name = food_name
+        food.proteins = proteins
+        food.carbs = carbs
+        food.fats = fats
+
+    else:
+        # creates instance of food model 
+        new_food = Food(
+            name=food_name,
+            proteins=proteins,
+            carbs=carbs,
+            fats=fats
+        )
+        db.session.add(new_food) #pushes instance of food model
     db.session.commit()      #into the database
     
     # redirects user to this page when submitted
     return redirect(url_for('main.add'))
+
+
+@main.route('/delete_food/<int:food_id>')
+def delete_food(food_id):
+    # finds where id equals in database and removes
+    food = Food.query.get(food_id)
+    db.session.delete(food)
+    db.session.commit()
+
+    return redirect(url_for('main.add'))
+
+
+
+@main.route('/edit_food/<int:food_id>')
+def edit_food(food_id):
+    # sets food and imports it into add.html
+    food = Food.query.get(food_id)
+    # list of all foods in database
+    foods = Food.query.all()
+    return render_template('add.html', food=food, foods=foods)
 
 
 @main.route('/view')
